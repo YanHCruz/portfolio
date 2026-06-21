e8a084e1781802fa4b7c76048cdf464990c961c5
YanCruz <yhenrique041@gmail.com>
2026-06-21 20:24:31 -0300

    Exclusão, e organização nos arquivos do projeto.

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './index.css'
+
+import  Cabecalho from '../../components/Cabecalho'
+import  Inicio from '../../components/Inicio'
+import Habilidades from '../../components/Habilidades'
+import  Sobre from '../../components/Sobre'
+import  Projetos from '../../components/Projetos'
+import Contato from '../../components/Contato'
+import  Rodape from '../../components/Rodape'
+
+export function App() {
+  return (
+
+    <main>
+
+      <Cabecalho />
+      <Inicio />
+      <Sobre /> 
+      <Habilidades /> 
+      <Projetos />
+      <Contato />
+      <Rodape />
+
+    </main>
+  );
+}

fb9a9a60e6c83c5a681f2ae634eecba009039a27
YanCruz <yhenrique041@gmail.com>
2026-06-22 01:20:19 -0300

    Alteração no App.tsx para funcionamento do deploy.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './index.css'
-import { useState, useEffect } from 'react'
 
 import  Cabecalho from '../components/Cabecalho'
 import  Inicio from '../components/Inicio'
@@ -10,31 +9,8 @@ import Contato from '../components/Contato'
 import  Rodape from '../components/Rodape'
 
 export function App() {
-  const [theme, setTheme] = useState(() => {
-    const savedTheme = localStorage.getItem('portfolio-theme');
-
-    return savedTheme ? savedTheme : 'dark';
-  });
-
-  useEffect(() => {
-    localStorage.setItem('portfolio-theme', theme);
-
-    if(theme === 'light') {
-      document.body.classList.add('light-mode');
-      document.body.classList.remove('dark-mode');
-    } else {
-      document.body.classList.add('dark-mode');
-      document.body.classList.remove('light-mode');
-    }
-  }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme((temaAtual) => (temaAtual === 'dark' ? 'light' : 'dark'));
-  };
   return (
-    <div>
-      <Cabecalho toggleTheme={toggleTheme} theme={theme} />
-    
+
     <main>
 
       <Cabecalho />
@@ -46,8 +22,5 @@ export function App() {
       <Rodape />
 
     </main>
-    </div>
   );
 }
-
-export default App;
